b9b9bfc9da08ea1c9364fac0e6dca30e2baaf0e4
COMPUBRODY <fealbemadera@gmail.com>
2022-02-17 00:52:00 -0600

    Primer Git en verilog

---
+++ counter.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 100ps
+module	counter #(parameter DATA_WIDTH=4)( 
+
+input clk, rst, ena, carry,
+output reg [3:0] Qdata,
+output reg	flag
+
+);
+
+
+
+always	@ (posedge clk)
+
+	begin
+
+		if (rst)
+		
+			Qdata	<= 0;
+			
+		else if(ena) begin
+		
+			Qdata <= Qdata + 1;
+			
+			if(Qdata == 4'h09)
+				begin
+					flag <= 1;
+					Qdata <= 4'h00;
+				end
+			else
+				flag <= 0;
+						
+		end
+		else if(carry)
+			
+			Qdata <= Qdata + 1;
+			if(Qdata == 4'h09)
+				begin
+					flag <= 1;
+				end
+			else
+				flag <= 0;			
+		
+		
+	end
+	
+
+	
+	
+
+endmodule
+
+
+		
+//			Qdata <= Qdata + 1;
+//					
+//			if(Qdata == 4'h09)
+//				begin
+//					#20 flag <= 1;
+//					#20 Qdata <= 4'h00;
+//				end
+//			else
+//				flag <= 0;
